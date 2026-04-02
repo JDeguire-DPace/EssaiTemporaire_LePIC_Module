e875c4efecc3d5b4b0e720ea2652b2d5ae02a3e2
Jasmin Deguire <Jasmin Deguire>
2026-04-02 15:04:58 -0400

    renouveau AI

---
+++ build/CMakeFiles/geometry_base.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "Fortran"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_Fortran
+  "/scratch/deguire1/EssaiTemporaire_LePIC_Module/modules/geometry/mod_domain.f90" "/scratch/deguire1/EssaiTemporaire_LePIC_Module/build/CMakeFiles/geometry_base.dir/modules/geometry/mod_domain.f90.o"
+  )
+set(CMAKE_Fortran_COMPILER_ID "IntelLLVM")
+set(CMAKE_Fortran_SUBMODULE_SEP "@")
+set(CMAKE_Fortran_SUBMODULE_EXT ".smod")
+
+# The include file search paths:
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH
+  "/scratch/deguire1/EssaiTemporaire_LePIC_Module/modules/legacy"
+  "mod"
+  "/cvmfs/soft.computecanada.ca/easybuild/software/2023/x86-64-v4/Compiler/intel2025/intelmpi/2021.16.1/mpi/2021.16/include/mpi"
+  "/cvmfs/soft.computecanada.ca/easybuild/software/2023/x86-64-v4/Compiler/intel2025/intelmpi/2021.16.1/mpi/2021.16/include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  "/scratch/deguire1/EssaiTemporaire_LePIC_Module/build/CMakeFiles/core_deps.dir/DependInfo.cmake"
+  "/scratch/deguire1/EssaiTemporaire_LePIC_Module/build/CMakeFiles/config_base.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "/scratch/deguire1/EssaiTemporaire_LePIC_Module/build/mod")
