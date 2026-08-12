14b066040f7e326e290bc3ba2c12ca406223991a
Jasmin Deguire <Jasmin Deguire>
2026-08-12 13:16:37 -0400

    EC_PIC2

--- build_profile/CMakeFiles/config_base.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/config_base.dir/modules/config/mod_config.f90.o"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang Fortran)
-  include(CMakeFiles/config_base.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
